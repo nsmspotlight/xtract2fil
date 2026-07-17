adf34938fe5b6fc0ac44a4b65cc56bdbfb08beb0
Ujjwal Panda <ujjwalpanda97@gmail.com>
2026-07-17 23:10:34 +0530

    ✨ Multiple changes bundled together.
    This commit comes with a couple of changes, including:
      1. Add support for IA beam data coming in from SPOTLIGHT.
      2. Add more checks when reading the headers.
      3. Make things SPOTLIGHT specific. This entails a few things, like:
        a. The directories are now created by `xtract2fil`.
        b. Many checks are now absorbed into `xtract2fil`.
        c. Dual mode is now default, and hence those options are removed.
      4. Make all coordinates J2000, rather than precessed.

--- src/xtract2fil/__main__.py
+++ src/xtract2fil/__main__.py
@@ -1,5 +1,7 @@
+import re
 import logging
 from pathlib import Path
+from typing import Generator
 from datetime import datetime
 from rich.console import Console
 from contextlib import ExitStack
@@ -13,7 +15,7 @@ from priwo import writehdr
 from astropy.time import Time
 from joblib import Parallel, delayed
 from rich.logging import RichHandler
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import SkyCoord, TETE
 
 app = cyclopts.App()
 app["--help"].group = "Admin"
@@ -30,7 +32,29 @@ logging.basicConfig(
 log = logging.getLogger("xtract2fil")
 
 
-def read_asciihdr(fn: str | Path) -> dict:
+class XtractionError(Exception):
+    pass
+
+
+def getmjd(t: datetime) -> float:
+    localtz = pytz.timezone("Asia/Kolkata")
+    localdt = localtz.localize(t, is_dst=None)
+    utcdt = localdt.astimezone(pytz.utc)
+    mjd = Time(utcdt).mjd
+    mjd = np.asarray(mjd)
+    mjd = float(mjd)
+    return mjd
+
+
+def inchunks(fx, N: int) -> Generator:
+    while True:
+        data = fx.read(N)
+        if (not data) or len(data) < N:
+            break
+        yield data
+
+
+def asciihdr(fn: str | Path) -> dict:
     hdr = {}
     extras = []
     with open(fn, "r") as lines:
@@ -56,21 +80,20 @@ def read_asciihdr(fn: str | Path) -> dict:
                     "Source DEC (Rad)": ("dec", float),
                     "Channels": ("nf", int),
                     "Bandwidth (MHz)": ("bw", float),
-                    "Frequency Ch. 0  (Hz)": ("f0", lambda x: float(x) * 1e-6),
                     "Channel width (Hz)": ("df", lambda x: float(x) * 1e-6),
+                    "Frequency Ch. 0  (Hz)": ("f0", lambda x: float(x) * 1e-6),
                     "Sampling time  (uSec)": ("dt", lambda x: float(x) * 1e-6),
-                    "Antenna mask pol1": ("maskX", lambda x: [int(_) == 1 for _ in np.binary_repr(int(x, 0))]),
                     "Antennas pol1": ("antX", lambda x: list(x.split())),
-                    "Antenna mask pol2": ("maskY", lambda x: [int(_) == 1 for _ in np.binary_repr(int(x, 0))]),
                     "Antennas pol2": ("antY", lambda x: list(x.split())),
+                    "Antenna mask pol1": ("maskX", lambda x: [int(_) == 1 for _ in np.binary_repr(int(x, 0))]),
+                    "Antenna mask pol2": ("maskY", lambda x: [int(_) == 1 for _ in np.binary_repr(int(x, 0))]),
                     "Beam mode": ("beammode", str),
                     "No. of stokes": ("npol", int),
                     "Num bits/sample": ("nbits", int),
+                    "Total No. of Beams": ("nbeams", int),
+                    "No. of PC Baselines": ("nbaselines", int),
+                    "Total No. of Beams/host": ("nbeamsperhost", int),
                     "De-Disperion DM": ("dm", lambda x: None if x == "NA" else float(x)),
-                    "PFB": ("pfb", bool),
-                    "PFB number of taps": ("pfbtaps", lambda x: None if x == "NA" else float(x)),
-                    "WALSH": ("walsh", lambda x: False if x == "OFF" else True),
-                    "Broad-band RFI Filter": ("rfifilter", lambda x: False if x == "OFF" else True),
                     "Date": ("istdate", str),
                     "IST Time": ("isttime", str),
                 }[key]
@@ -78,6 +101,13 @@ def read_asciihdr(fn: str | Path) -> dict:
                 hdr[name] = conv(val)
             except KeyError:
                 pass
+
+    if hdr.get("istdate", None) is None:
+        raise XtractionError("DATE FIELD MISSING. FILE CORRUPTED. ABORT.")
+    if hdr.get("isttime", None) is None:
+        raise XtractionError("IST TIME FIELD MISSING. FILE CORRUPTED. ABORT.")
+    if hdr.get("nbeams", None) is None:
+        raise XtractionError("NBEAMS FIELD MISSING. FILE CORRUPTED. ABORT.")
 
     fields = []
     header = extras[1].split()[2:]
@@ -90,109 +120,164 @@ def read_asciihdr(fn: str | Path) -> dict:
     return hdr
 
 
-def getmjd(t: datetime):
-    localtz = pytz.timezone("Asia/Kolkata")
-    localdt = localtz.localize(t, is_dst=None)
-    utcdt = localdt.astimezone(pytz.utc)
-    mjd = Time(utcdt).mjd
-    return mjd
+def ra2flt(coords: SkyCoord) -> float:
+    ra_d, ra_m, ra_s = getattr(getattr(coords, "ra"), "hms")
+    ra_d = int(ra_d)
+    ra_m = int(ra_m)
+    ra_s = float(ra_s)
+    if ra_d < 0.0:
+        ra_m = -ra_m
+        ra_s = -ra_s
+    ra_d = str(ra_d).zfill(2)
+    ra_m = str(ra_m).zfill(2)
+    ra_si, ra_sf = str(ra_s).split(".")
+    ra_s = ".".join([ra_si.zfill(2), ra_sf])
+    ra_f = float("".join([ra_d, ra_m, ra_s]))
+    return ra_f
 
 
-def inchunks(fx, N: int):
-    while True:
-        data = fx.read(N)
-        if (not data) or len(data) < N:
-            break
-        yield data
+def dec2flt(coords: SkyCoord) -> float:
+    dec_d, dec_m, dec_s = getattr(getattr(coords, "dec"), "dms")
+    dec_d = int(dec_d)
+    dec_m = int(dec_m)
+    dec_s = float(dec_s)
+    if dec_m < 0.0:
+        dec_m = -dec_m
+    if dec_s < 0.0:
+        dec_s = -dec_s
+    dec_d = str(dec_d).zfill(2)
+    dec_m = str(dec_m).zfill(2)
+    dec_si, dec_sf = str(dec_s).split(".")
+    dec_s = ".".join([dec_si.zfill(2), dec_sf])
+    dec_f = float("".join([dec_d, dec_m, dec_s]))
+    return dec_f
 
 
-def xtract2fil(
-    fn: str | Path,
-    nbeams: int,
-    outdir: str | Path,
-    scan: str = "",
-    fbin: int = 1,
-    tbin: int = 1,
-    offset: int = 64,
-    dual: bool = False,
-):
-    fn = Path(fn)
-    if dual: # In addition to the full resolution data, also write downsampled data.
-        outdir_dwnsmp = Path(outdir)/("FilData_dwnsmp/" + scan)
-        if not outdir_dwnsmp.exists():
-            outdir_dwnsmp.mkdir(parents=True, exist_ok=True)
-        outdir = Path(outdir)/("FilData/" + scan)
-    else:
-        outdir = Path(outdir)
-    if not outdir.exists():
-        outdir.mkdir(parents=True, exist_ok=True)
-    downflag = (fbin > 1) or (tbin > 1)
+def iaxtract(fn: Path) -> None:
+    hdr = asciihdr(fn.with_suffix(".0.ahdr"))
+    data = np.fromfile(fn, dtype=np.uint8)
+    data = data.reshape(-1, hdr["nf"])
 
-    hdr = read_asciihdr(str(fn) + ".ahdr")
-
-    df = hdr["df"]
-    bw = hdr["bw"]
-    dt = hdr["dt"]
     nf = hdr["nf"]
+    fh = hdr["f0"]
+    df = hdr["df"]
+    dt = hdr["dt"]
     nbits = hdr["nbits"]
     fname = str(fn.name)
     source = hdr["source"]
-    radecs = hdr["coords"]
     mjd = getmjd(hdr["istdatetime"])
-    flip = True if df > 0.0 else False
 
-    fh = hdr["f0"]
-    if flip:
-        df = -df
-        fh = hdr["f0"] + bw - (0.5 * df)
+    rad = getattr(u, "rad")
+    obstime = Time(mjd, format="mjd")
+    coords = SkyCoord(
+        hdr["ra"] * rad,
+        hdr["dec"] * rad,
+        frame=TETE(obstime=obstime),
+    ).transform_to("icrs")
+    ra_f = ra2flt(coords)
+    dec_f = dec2flt(coords)
 
     nblks = 32
     defaultdt = 1.31072e-3
     blktime = 800 * defaultdt
     nt = int(round(blktime / dt))
     slicesize = nf * nt * nblks
+
+    fbin = int(nf / 1024)
+    tbin = int(13.1072e-3 / dt)
     if (nf % fbin) != 0:
         raise ValueError(f"fbin={fbin} must be a factor of nf={nf}")
     elif (nt % tbin) != 0:
         raise ValueError(f"tbin={tbin} must be a factor of nt={nt}")
-    else:
-        downflag = (fbin > 1) or (tbin > 1)
+
+    filhdr = {
+        "rawdatafile": fname,
+        "source_name": source,
+        "az_start": 0.0,
+        "za_start": 0.0,
+        "src_raj": ra_f,
+        "src_dej": dec_f,
+        "tstart": mjd,
+        "tsamp": dt,
+        "fch1": fh,
+        "foff": df,
+        "nchans": nf,
+        "telescope_id": 7,
+        "machine_id": 14,
+        "data_type": 1,
+        "ibeam": 1,
+        "nbeams": 1,
+        "nbits": nbits,
+        "barycentric": 0,
+        "pulsarcentric": 0,
+        "nifs": 1,
+        "size": 0,
+    }
+
+    writehdr(filhdr, fn.with_suffix(".fil"))
+
+    filhdr["foff"] = df * fbin
+    filhdr["tsamp"] = dt * tbin
+    filhdr["nchans"] = int(round(nf / fbin))
+    writehdr(filhdr, fn.with_suffix(".down.fil"))
+
+    with ExitStack() as stack:
+        filfile = stack.enter_context(open(fn.with_suffix(".fil"), "ab"))
+        dwnfile = stack.enter_context(open(fn.with_suffix(".down.fil"), "ab"))
+        with open(fn, "rb") as f:
+            for data in inchunks(f, slicesize):
+                array = np.frombuffer(data, dtype=np.uint8)
+                array = array.reshape(-1, nf)
+                array.tofile(filfile)
+
+                array = array.reshape((-1, int(array.shape[1] // fbin), fbin)).mean(2)
+                array = array.reshape((-1, tbin, array.shape[1])).mean(1)
+                array = array.astype(np.uint8)
+                array.tofile(dwnfile)
+
+
+def pcxtract(fn: Path, fildir: Path, dwndir: Path):
+    hdr = asciihdr(str(fn) + ".ahdr")
+
+    nbeams = hdr["nbeamsperhost"]
+
+    nf = hdr["nf"]
+    fh = hdr["f0"]
+    df = hdr["df"]
+    dt = hdr["dt"]
+    nbits = hdr["nbits"]
+    fname = str(fn.name)
+    source = hdr["source"]
+    radecs = hdr["coords"]
+    mjd = getmjd(hdr["istdatetime"])
+
+    nblks = 32
+    defaultdt = 1.31072e-3
+    blktime = 800 * defaultdt
+    nt = int(round(blktime / dt))
+    slicesize = nf * nt * nblks
+
+    fbin = int(nf / 1024)
+    tbin = int(13.1072e-3 / dt)
+    if (nf % fbin) != 0:
+        raise ValueError(f"fbin={fbin} must be a factor of nf={nf}")
+    elif (nt % tbin) != 0:
+        raise ValueError(f"tbin={tbin} must be a factor of nt={nt}")
 
     rad = getattr(u, "rad")
     beamix = radecs["BM-Idx"].to_numpy(dtype=int)
-    filpaths = [outdir / f"BM{ix}.fil" for ix in beamix]
-    if dual:
-        filpaths_dwnsmp = [outdir_dwnsmp / f"BM{ix}.down.fil" for ix in beamix]
+    filpaths = [fildir / f"BM{ix}.fil" for ix in beamix]
+    dwnpaths = [dwndir / f"BM{ix}.down.fil" for ix in beamix]
     for ix, filpath in enumerate(filpaths):
-        coords = SkyCoord(radecs.iloc[ix]["RA"] * rad, radecs.iloc[ix]["DEC"] * rad)
-
-        ra_d, ra_m, ra_s = getattr(getattr(coords, "ra"), "hms")
-        ra_d = int(ra_d)
-        ra_m = int(ra_m)
-        ra_s = float(ra_s)
-        if ra_d < 0.0:
-            ra_m = -ra_m
-            ra_s = -ra_s
-        ra_d = str(ra_d).zfill(2)
-        ra_m = str(ra_m).zfill(2)
-        ra_si, ra_sf = str(ra_s).split(".")
-        ra_s = ".".join([ra_si.zfill(2), ra_sf])
-        ra_sigproc = float("".join([ra_d, ra_m, ra_s]))
-
-        dec_d, dec_m, dec_s = getattr(getattr(coords, "dec"), "dms")
-        dec_d = int(dec_d)
-        dec_m = int(dec_m)
-        dec_s = float(dec_s)
-        if dec_m < 0.0:
-            dec_m = -dec_m
-        if dec_s < 0.0:
-            dec_s = -dec_s
-        dec_d = str(dec_d).zfill(2)
-        dec_m = str(dec_m).zfill(2)
-        dec_si, dec_sf = str(dec_s).split(".")
-        dec_s = ".".join([dec_si.zfill(2), dec_sf])
-        dec_sigproc = float("".join([dec_d, dec_m, dec_s]))
-        fil_hdr={
+        obstime = Time(mjd, format="mjd")
+        coords = SkyCoord(
+            radecs.iloc[ix]["RA"] * rad,
+            radecs.iloc[ix]["DEC"] * rad,
+            frame=TETE(obstime=obstime),
+        ).transform_to("icrs")
+        ra_f = ra2flt(coords)
+        dec_f = dec2flt(coords)
+        filhdr = {
             "rawdatafile": fname,
             "source_name": source,
             "nifs": 1,
@@ -207,85 +292,108 @@ def xtract2fil(
             "fch1": fh,
             "tsamp": dt,
             "nchans": nf,
-            "src_raj": ra_sigproc,
-            "src_dej": dec_sigproc,
+            "src_raj": ra_f,
+            "src_dej": dec_f,
             "size": 0,
         }
-        if dual: # Write both the full resolution as well as the downsampled data.
-            writehdr(fil_hdr, str(filpath))
-            fil_hdr["tsamp"] = dt * tbin
-            fil_hdr["foff"] = df * fbin
-            fil_hdr["nchans"] = int(round(nf / fbin))
-            writehdr(fil_hdr, str(filpaths_dwnsmp[ix]))
-        else: # Write either the full resolution or the downsampled data.
-            fil_hdr["tsamp"] = dt * tbin
-            fil_hdr["foff"] = df * fbin
-            fil_hdr["nchans"] = int(round(nf / fbin))
-            writehdr(fil_hdr, str(filpath))
+        writehdr(filhdr, str(filpath))
+
+        filhdr["foff"] = df * fbin
+        filhdr["tsamp"] = dt * tbin
+        filhdr["nchans"] = int(round(nf / fbin))
+        writehdr(filhdr, str(dwnpaths[ix]))
 
     with ExitStack() as stack:
         filfiles = [stack.enter_context(open(_, "ab")) for _ in filpaths]
-        if dual:
-            filfiles_dwnsmp = [stack.enter_context(open(_, "ab")) for _ in filpaths_dwnsmp]
-        with open(fn, "rb") as fx:
-            for ix, data in enumerate(inchunks(fx, slicesize)):
-                filfile = filfiles[ix % nbeams]
-                if offset != 0:
-                    array = np.frombuffer(data, dtype=np.int8)
-                    array = array.astype(np.int16) + offset
-                    array = array.astype(np.uint8)
-                else:
-                    array = np.frombuffer(data, dtype=np.uint8)
+        dwnfiles = [stack.enter_context(open(_, "ab")) for _ in dwnpaths]
+        with open(fn, "rb") as f:
+            for ix, data in enumerate(inchunks(f, slicesize)):
+                array = np.frombuffer(data, dtype=np.uint8)
                 array = array.reshape(-1, nf)
-                if flip:
-                    array = np.fliplr(array)
-                if dual: # Write both the full resolution as well as the downsampled data.
-                    array.tofile(filfile) # First, write the full resolution data.
-                    filfile_dwnsmp = filfiles_dwnsmp[ix % nbeams]
-                    array = array.reshape((-1, int(array.shape[1] // fbin), fbin)).mean(2)
-                    array = array.reshape((-1, tbin, array.shape[1])).mean(1)
-                    array = array.astype(np.uint8)
-                    array.tofile(filfile_dwnsmp) # Then, write the downsampled data.
-                elif downflag: # Write the downsampled data only.
-                    array = array.reshape((-1, int(array.shape[1] // fbin), fbin)).mean(2)
-                    array = array.reshape((-1, tbin, array.shape[1])).mean(1)
-                    array = array.astype(np.uint8)
-                    array.tofile(filfile_dwnsmp)
-                else: # Write the full resolution data only.
-                    array.tofile(filfile)
+                array.tofile(filfiles[ix % nbeams])
+
+                array = array.reshape((-1, int(array.shape[1] // fbin), fbin)).mean(2)
+                array = array.reshape((-1, tbin, array.shape[1])).mean(1)
+                array = array.astype(np.uint8)
+                array.tofile(dwnfiles[ix % nbeams])
 
 
-@app.default
-def main(
-    files: list[str | Path],
-    /,
-    fbin: int = 1,
-    tbin: int = 1,
-    njobs: int = -1,
-    nbeams: int = 10,
-    offset: int = 64,
-    dual: bool = True,
-    output: str | Path = Path.cwd(),
-    scan: str = "",
-):
-    njobs = njobs if njobs > 0 else len(files)
-    if dual and scan == "":
-        raise ValueError("Please provide a scan name using --scan")
-    log.info(f"Xtracting {nbeams} beams from {len(files)} files...")
-    log.info(f"Using {njobs} cores")
-    Parallel(n_jobs=njobs)(
-        delayed(xtract2fil)(
-            fn=f,
-            fbin=fbin,
-            tbin=tbin,
-            nbeams=nbeams,
-            outdir=output,
-            offset=offset,
-            dual=dual,
-            scan=scan,
+@app.command
+def ia(obsname: str):
+    obsdir = Path("/lustre_data/spotlight/data") / obsname
+    scans = set([_.name.split(".")[0] for _ in (obsdir / "IABeamData").glob("*.raw*")])
+
+    rxraw = re.compile(r"\.raw\.\d+$")
+    rxhdr = re.compile(r"\.raw\.\d+\.ahdr$")
+
+    rawfiles = [
+        f
+        for scan in scans
+        if (f := obsdir / "BeamData" / f"{scan}.raw.*").is_file()
+        and rxraw.search(f.name)
+    ]
+
+    hdrfiles = [
+        f
+        for scan in scans
+        if (f := obsdir / "BeamData" / f"{scan}.raw.*.ahdr").is_file()
+        and rxhdr.search(f.name)
+    ]
+
+    if (len(rawfiles) != len(scans)) or (len(hdrfiles) != len(scans)):
+        raise XtractionError("MISSING FILES. ABORT.")
+    if len(rawfiles) != len(hdrfiles):
+        raise XtractionError("NUMBER OF RAW AND HDR FILES NOT EQUAL. ABORT.")
+
+    njobs = len(scans)
+    log.info(f"Xtracting IA beam data for {obsdir}...")
+    log.info(f"Number of rawfiles = {len(rawfiles):d}.")
+    log.info(f"Number of cores being used = {njobs:d} cores.")
+    Parallel(n_jobs=njobs)(delayed(iaxtract)(fn=fn) for fn in rawfiles)
+
+
+@app.command
+def pc(obsname: str):
+    nhosts = 16
+    obsdir = Path("/lustre_data/spotlight/data") / obsname
+    scans = set([_.name.split(".")[0] for _ in (obsdir / "BeamData").glob("*.raw*")])
+
+    rxraw = re.compile(r"\.raw\.\d+$")
+    rxhdr = re.compile(r"\.raw\.\d+\.ahdr$")
+    log.info(f"Xtracting PC beam data for {obsdir}...")
+    for scan in scans:
+        rawfiles = [
+            f
+            for f in (obsdir / "BeamData").glob(f"{scan}.raw.*")
+            if f.is_file() and rxraw.search(f.name)
+        ]
+        hdrfiles = [
+            f
+            for f in (obsdir / "BeamData").glob(f"{scan}.raw.*.ahdr")
+            if f.is_file() and rxhdr.search(f.name)
+        ]
+        if (len(rawfiles) != nhosts) or (len(hdrfiles) != nhosts):
+            raise XtractionError("NOT ENOUGH FILES. ABORT.")
+        if len(rawfiles) != len(hdrfiles):
+            raise XtractionError("NUMBER OF RAW AND HDR FILES NOT EQUAL. ABORT.")
+
+        fildir = Path(obsdir) / "FilData" / scan
+        dwndir = Path(obsdir) / "FilData_dwnsmp" / scan
+        fildir.mkdir(exist_ok=True)
+        dwndir.mkdir(exist_ok=True)
+
+        njobs = len(rawfiles)
+        log.info(f"Xtracting data for {scan}...")
+        log.info(f"Number of rawfiles = {len(rawfiles):d}.")
+        log.info(f"Number of cores being used = {njobs:d} cores.")
+        Parallel(n_jobs=njobs)(
+            delayed(pcxtract)(
+                fn=fn,
+                fildir=fildir,
+                dwndir=dwndir,
+            )
+            for fn in rawfiles
         )
-        for f in files
-    )
 
 
 if __name__ == "__main__":
